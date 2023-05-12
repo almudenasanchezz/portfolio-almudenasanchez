23eb628172455883dbd0b03ef7fe52ff8d73e515
almudenasanchezz <sanchezgallegoalmudena@gmail.com>
2023-05-12 18:14:34 +0200

    improvement of the landing and creation of some scss files

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,5 +1,17 @@
 function Landing() {
-  return <div>Está es mi landing</div>;
+  return (
+    <section className="landing">
+      <h1 className="h1">FRONTEND DEVELOPER</h1>
+      <div className="info">
+        <p className="info-text">ALMUDENA SÁNCHEZ</p>
+        <p className="info-text">BASED IN SPAIN</p>
+      </div>
+      <div className="buttons">
+        <button className="primary-button">Contact now</button>
+        <button className="secondary-button">Download CV</button>
+      </div>
+    </section>
+  );
 }
 
 export default Landing;
