9eb12a6cf6c827e5cf0096ea1582012575e4d24f
almudenasanchezz <sanchezgallegoalmudena@gmail.com>
2023-05-12 11:47:38 +0200

    header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,51 @@ import "./scss/global.scss";
 function App() {
   return (
     <>
-      <header className="menu">
-        <nav className="navegation">
-          <Link className="link" to="/list">
+      <header className="header">
+        <nav className="header-nav">
+          <Link className="header-link" to="/list">
             Home
           </Link>
-          <Link className="link" to="/list">
+          <Link className="header-link" to="/list">
             Projects
           </Link>
-          <Link className="link" to="/list">
+          <Link className="header-link" to="/list">
             About
           </Link>
-          <Link className="link" to="/list">
+          <Link className="header-link" to="/list">
             Contact
           </Link>
         </nav>
       </header>
       <Outlet />
+      <footer className="footer">
+        <div className="logo">MI LOGO</div>
+        <nav className="footer-nav">
+          <Link className="footer-link" to="/list">
+            Github
+          </Link>
+          <Link className="footer-link" to="/list">
+            LinkedIn
+          </Link>
+          <Link className="footer-link" to="/list">
+            Behance
+          </Link>
+        </nav>
+        <nav className="footer-nav">
+          <Link className="footer-link" to="/list">
+            Home
+          </Link>
+          <Link className="footer-link" to="/list">
+            Projects
+          </Link>
+          <Link className="footer-link" to="/list">
+            About
+          </Link>
+          <Link className="footer-link" to="/list">
+            Contact
+          </Link>
+        </nav>
+      </footer>
     </>
   );
 }
