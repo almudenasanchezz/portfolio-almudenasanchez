c048cf6d413dfbf6b6cf8ea1afb3e1849922c214
almudenasanchezz <sanchezgallegoalmudena@gmail.com>
2023-05-19 14:04:15 +0200

    structure about me page

--- src/views/about/About.jsx
+++ src/views/about/About.jsx
@@ -4,6 +4,51 @@ function About() {
   return (
     <section className="landing">
       <h2 className="h2">ABOUT</h2>
+      <div className="intro__about">
+        <div className="intro__about__left"></div>
+        <div className="intro__about__right">
+          <div className="intro__about__right__photo"></div>
+          <div className="intro__about__right__text">
+            I am a frontend web developer and ui designer with a background as
+            an engineer. I have always been passionate about technology and
+            design, which is why I enjoy creating attractive and accessible
+            websites. I am passionate about learning new things and improving
+            myself.
+          </div>
+        </div>
+      </div>
+
+      <h2 className="h2">HARD SKILLS</h2>
+      <div className="hs__container">
+        <div className="hskill">
+          <div className="hskill__color-red"></div>
+          <div className="hskill__info">
+            <p className="hskill__info__text">HTML</p>
+          </div>
+        </div>
+        <div className="hskill">
+          <div className="hskill__color-green"></div>
+          <div className="hskill__info">
+            <p className="hskill__info__text">CSS</p>
+            <p className="hskill__info__text">SCSS</p>
+          </div>
+        </div>
+        <div className="hskill">
+          <div className="hskill__color-pink"></div>
+          <div className="hskill__info">
+            <p className="hskill__info__text">JAVASCRIPT</p>
+            <p className="hskill__info__text">REACT</p>
+          </div>
+        </div>
+        <div className="hskill">
+          <div className="hskill__color-blue"></div>
+          <div className="hskill__info">
+            <p className="hskill__info__text">FIGMA</p>
+            <p className="hskill__info__text">ADOBE</p>
+          </div>
+        </div>
+      </div>
+      <h2 className="h2">SOFT SKILLS</h2>
     </section>
   );
 }
--- src/views/contact/Contact.jsx
+++ src/views/contact/Contact.jsx
@@ -3,7 +3,10 @@ import "./contact.scss";
 function Contact() {
   return (
     <section className="landing">
-      <h2 className="h2">LET’S WORK TOGETHER</h2>
+      <h2 className="h2 centered">SEND ME A MESSAGE</h2>
+      <a className="email centered" href="">
+        sanchezgallegoalmudena@gmail.com
+      </a>
     </section>
   );
 }
