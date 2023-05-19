602676223dff36280fa02b0298718c2863cafa57
almudenasanchezz <sanchezgallegoalmudena@gmail.com>
2023-05-19 18:40:44 +0200

    contact page

--- src/views/contact/Contact.jsx
+++ src/views/contact/Contact.jsx
@@ -2,9 +2,9 @@ import "./contact.scss";
 
 function Contact() {
   return (
-    <section className="landing">
-      <h2 className="h2 centered">SEND ME A MESSAGE</h2>
-      <a className="email centered" href="">
+    <section className="landing centered">
+      <h2 className="h2 contact">SEND ME A MESSAGE</h2>
+      <a className="email" href="mailto:sanchezgallegoalmudena@gmail.com">
         sanchezgallegoalmudena@gmail.com
       </a>
     </section>
