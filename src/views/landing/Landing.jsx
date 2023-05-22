50e597dfbdaa2b7035086686b21b85fa51e16f22
almudenasanchezz <sanchezgallegoalmudena@gmail.com>
2023-05-22 20:08:53 +0200

    projects section on the home page

--- src/views/landing/Landing.jsx
+++ src/views/landing/Landing.jsx
@@ -61,6 +61,56 @@ function Landing() {
           </div>
         </div>
       </section>
+      <section className="landing">
+        <h2 className="h2">PROJECTS</h2>
+        <div className="projects__container">
+          <div className="project">
+            <div className="project__image"></div>
+            <div className="project__content">
+              <div className="project__title">
+                <p className="project__title__language">Javascript | React</p>
+                <p className="project__title__exercise">1. To do list</p>
+                <div className="project__btn__container">
+                  <p className="project__btn__container__icon">icono</p>
+                  <button className="project__btn__container__btn">
+                    View project
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="project">
+            <div className="project__content">
+              <div className="project__title">
+                <p className="project__title__language">React</p>
+                <p className="project__title__exercise">2. Calculator</p>
+                <div className="project__btn__container">
+                  <p className="project__btn__container__icon">icono</p>
+                  <button className="project__btn__container__btn">
+                    View project
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="project__image"></div>
+          </div>
+          <div className="project">
+            <div className="project__image"></div>
+            <div className="project__content">
+              <div className="project__title">
+                <p className="project__title__language">Figma</p>
+                <p className="project__title__exercise">3. Web redesign</p>
+                <div className="project__btn__container">
+                  <p className="project__btn__container__icon">icono</p>
+                  <button className="project__btn__container__btn">
+                    View project
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
