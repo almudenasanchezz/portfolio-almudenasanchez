423c35c63c6a7a7bcdbf1d7a38c525bc316dc001
almudenasanchezz <sanchezgallegoalmudena@gmail.com>
2023-06-02 17:38:29 +0200

    dropdown menu mobile

--- src/components/header/Topbar.jsx
+++ src/components/header/Topbar.jsx
@@ -1,14 +1,31 @@
 import "./Topbar.scss";
 import TopbarLink from "./TopbarLink";
+import { useState } from "react";
 
 const Topbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  const handleShowMenuClick = () => {
+    setShowMobileMenu(!showMobileMenu);
+  };
+
   return (
-    <nav className="topbar__nav">
-      <TopbarLink text="Home" to={`/`} />
-      <TopbarLink text="Projects" to={`/projects`} />
-      <TopbarLink text="About" to={`/about`} />
-      <TopbarLink text="Contact" to={`/contact`} />
-    </nav>
+    <>
+      <nav
+        className={`topbar__nav ${showMobileMenu ? "topbar__nav__show" : ""}`}
+      >
+        <button className="topbar__nav__close" onClick={handleShowMenuClick}>
+          Close
+        </button>
+        <TopbarLink text="Home" to={`/`} />
+        <TopbarLink text="Projects" to={`/projects`} />
+        <TopbarLink text="About" to={`/about`} />
+        <TopbarLink text="Contact" to={`/contact`} />
+      </nav>
+      <button className="show__menu" onClick={handleShowMenuClick}>
+        Menu
+      </button>
+    </>
   );
 };
 
